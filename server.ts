8f48248b55187c7716d9c1bdd938e662261f0de3
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-08-19 14:10:38 +0800

    feat: created server app for database

---
+++ server.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import app from "./app";
+const DB = process.env.MONGO_URI;
+const PORT = process.env.PORT;
+const connect = async () => {
+  try {
+    await mongoose.connect(DB!, {
+      dbName: "mern",
+    });
+    console.log("Database Connected!");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+connect();
