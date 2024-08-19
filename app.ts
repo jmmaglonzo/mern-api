21bd2e8ccbce04a001ed522052778c2074cb27db
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-08-19 14:10:25 +0800

    feat: initialize express app

---
+++ app.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import cors from "cors";
+import userRouter from "./src/routes/route";
+import routeHandler from "./src/middleware/routeHandler";
+import errorHandler from "./src/middleware/errorHandler";
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+const isDevelopment = process.env.NODE_ENV === "development";
+
+const corsOptions = {
+  origin: isDevelopment
+    ? "http://localhost:3000"
+    : process.env.PRODUCTION_CLIENT_URL,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.use("/api/user", userRouter);
+
+app.all("*", routeHandler);
+app.use(errorHandler);
+
+export default app;
